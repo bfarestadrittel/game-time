c203d759248a2ffad3e20a08f1bbb9b967e72363
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-10-12 09:49:50 -0600

    update testing suite

--- test/BrickArray-test.js
+++ test/BrickArray-test.js
@@ -8,34 +8,47 @@ const BrickArray = require('../lib/BrickArray.js');
 describe('BrickArray unit testing', () => {
 
   it('should be a function', () => {
-    let BrickArray = new BrickArray();
+    let brickArray = new BrickArray();
   })
 
-  it('should have an x position', () => {
-
-  })
-
-  it('should have a y position', () => {
-
-  })
+  it('should instantiate a BrickArray', () => {
+  var brickArray = new BrickArray();
+  assert.isObject(brickArray);
+  });
 
   it('should have 21 total bricks', () => {
+    let brickArray = new BrickArray();
 
+    assert.equal(array === [i]);
   })
 
   it('should have 3 rows', () => {
+    let brickArray = new BrickArray(7, 3);
 
+    assert.equal(brickArray.bricks.rows === 3);
   })
 
   it('should have 7 columns', () => {
+    let brickArray = new BrickArray(7, 3);
 
+    assert.equal(brickArray.bricks.columns === 7);
   })
 
-  it('should remove brick on contact with ball', () => {
+  it('should remove a brick from array after a collisions', () => {
+    let brick = new Brick(100, 20);
 
+    assert.equal(bricks.brick.w, 100);
+    assert.equal(bricks.brick.h, 20);
+    brickArray.removeBricks();
+    assert.equal(brickArray.bricks.brick = [0]);
+    // assert.equal(brick.w, 0);
+    // assert.equal(brick.h, 0);
   })
 
-  it('should update total number of bricks', () => {
+  it('should have win if there are no bricks in the array', () => {
+    let brickArray = new BrickArray(7, 3);
 
+    assert.equal(brickArray.bricks.columns === 7);
   })
+
 })
--- test/Paddle-test.js
+++ test/Paddle-test.js
@@ -7,42 +7,48 @@ const BrickArray = require('../lib/BrickArray.js');
 
 describe('paddle unit testing', () => {
 
-  it('should be a function', () => {
+  it('it should be a function', () => {
     let paddle = new Paddle();
-  })
+  });
 
-  it('should move right', () => {
+  it('should instantiate a Paddle', () => {
+  var paddle = new Paddle();
+  assert.isObject(paddle);
+  });
+
+  it('it should move right', () => {
     let paddle = new Paddle(350, 580);
-    let x = paddle.x;
 
-    assert.equal(x === 350, true);
-    paddle.keyDownHandler(event);
-    assert.equal(x === x + 25, true);
-  })
+    assert.equal(paddle.x === 350, true);
+    assert.equal(paddle.x += 25, true);
+    assert.equal(paddle.x === 375, true);
+  });
 
-  it('should move right', () => {
+  it('it should move left', () => {
     let paddle = new Paddle(350, 580);
-    let x = paddle.x;
 
-    assert.equal(y === 350, true);
-    paddle.keyDownHandler(event);
-    assert.equal(x === x - 25, true);
-  })
+    assert.equal(paddle.x === 350, true);
+    if (event.code === 'ArrowRight' && this.x < 1400 - this.x) {
+    assert.equal(paddle.x -= 25, true);
+    }
+  });
 
-  // it('should stay inside the canvas walls', () => {
-  //   let paddle = new Paddle(350, 580);
-  //   let x = paddle.x;
-  //
-  //   if x <= 0
-  //    moveRight === false;
-  //   assert.equal(x === x = 25, true);
-  //   &&
-  //   if x + >= 800
-  //    moveLeft === false;
-  // })
+  it('it should not go off the left side of the canvas', () => {
+    let paddle = new Paddle(0, 580);
 
-  //  it('should gain power ups', () => {
-  //    if powerUp === paddle.x + width;
-  //    return paddle.powerUp();
-  //})
+    if(paddle.x <= 0) {
+      paddle.moveLeft === false;
+      assert.equal(paddle.x === paddle.x + 10);
+    }
+  });
+
+  it('it should not go off the right side of the canvas', () => {
+    let paddle = new Paddle(0, 580);
+
+    if(paddle.x >= 800) {
+      paddle.moveRight === false;
+      assert.equal(paddle.x === paddle.x - 10);
+    }
+  });
+
 })
