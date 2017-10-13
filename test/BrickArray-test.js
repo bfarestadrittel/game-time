47122b4a78addb3d8d71ad1fe9c2cc5031d154d8
Matthew Arvidson <Matthew.Arvidson@gmail.com>
2017-10-13 13:19:49 -0600

    testing back up to speed

--- test/BrickArray-test.js
+++ test/BrickArray-test.js
@@ -4,6 +4,7 @@ const Ball = require('../lib/Ball.js');
 const Paddle = require('../lib/Paddle.js');
 const Brick = require('../lib/Brick.js');
 const BrickArray = require('../lib/BrickArray.js');
+const Game = require('../lib/Game.js');
 
 describe('BrickArray unit testing', () => {
 
@@ -12,40 +13,48 @@ describe('BrickArray unit testing', () => {
   })
 
   it('should instantiate a BrickArray', () => {
-    var brickArray = new BrickArray();
-    assert.isObject(brickArray);
+  var brickArray = new BrickArray();
+  assert.isObject(brickArray);
   });
+
+  it('should have 21 total bricks', () => {
+    let brickArray = new BrickArray(7, 3);
+    brickArray.buildBricks()
+    assert.equal(brickArray.bricks.length, 21);
+  })
 
   it('should have 3 rows', () => {
     let brickArray = new BrickArray(7, 3);
-
-    assert.equal(brickArray.row === 3, true);
+    brickArray.buildBricks()
+    assert.equal(brickArray.row, 3);
   })
 
   it('should have 7 columns', () => {
     let brickArray = new BrickArray(7, 3);
-
-    assert.equal(brickArray.column === 7, true);
+    brickArray.buildBricks()
+    assert.equal(brickArray.column, 7);
   })
 
-//   it('should remove a brick from array after a collisions', () => {
-//     let ball = new Ball(10, 10);
-//     let brickArray = brick.showBricks();
-//     let brick = new Brick(10, 10);
-// console.log(brickArray.bricks.length);
-//     ball.move();
-//     brickArray.showBricks();
-//     assert.equal(brickArray.bricks.length, 21);
-//     brickArray.removeBricks(brickArray, ball, new Game);
-//     assert.equal(brickArray.bricks.length, 20);
-//   })
-//
-//   it.skip('should win if there are no bricks in the array', () => {
-//     let brickArray = new BrickArray(7, 3);
-//     let game = new Game();
-//
-//     if(game.score === 2100) {
-//       this.winGameScreen();
-//     }
-//   })
-});
+  it('should remove a brick from array after a collision', () => {
+      let ball = new Ball(25, 25);
+      let brickArray = new BrickArray(7, 3);
+
+      brickArray.buildBricks();
+
+      console.log(brickArray.bricks.length)
+      assert.equal(brickArray.bricks.length, 21);
+
+      console.log(brickArray.bricks[0]);
+      console.log(ball);
+      brickArray.removeBricks(brickArray.bricks, ball, new Game);
+      console.log(ball.x);
+      assert.equal(brickArray.bricks.length, 20);
+    })
+
+  // it('should have win if there are no bricks in the array', () => {
+  //   let brickArray = new BrickArray(7, 3);
+  //
+  //   assert.equal(brickArray.bricks.columns === 7);
+  // })
+
+})
